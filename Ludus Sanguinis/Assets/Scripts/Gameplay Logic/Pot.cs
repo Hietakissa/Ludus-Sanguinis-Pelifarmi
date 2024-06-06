cccb73e26eeb697239ddd44af9a01783cca36a34
hietakissa <pyrypoikolainen@gmail.com>
2024-06-06 14:14:07 +0300

    pot throwing?

--- Ludus Sanguinis/Assets/Scripts/Gameplay Logic/Pot.cs
+++ Ludus Sanguinis/Assets/Scripts/Gameplay Logic/Pot.cs
@@ -8,21 +8,28 @@ public class Pot : MonoBehaviour
     [SerializeField] Transform visualSpawnPos;
     [SerializeField] GameObject chipPrefab;
     [SerializeField] SoundContainer potDingDingSound;
+    [SerializeField] Transform chipThrowPos;
+    [SerializeField] float throwForce;
 
     public bool IsThrowing { get; private set; }
 
     public int Capacity { get; private set; }
     public int FillAmount { get; private set; }
 
+    int totalAddedChips;
+
+
     public void SetCapacity(int capacity)
     {
         Capacity = capacity;
+        totalAddedChips = 0;
     }
 
     public IEnumerator AddValue(int value)
     {
         Debug.Log($"adding {value} to pot; value after: {FillAmount + value}/{Capacity}");
 
+        totalAddedChips += value;
         int overflowTimes = 0;
         for (int i = 0; i < value; i++)
         {
@@ -61,6 +68,14 @@ public class Pot : MonoBehaviour
         {
             IsThrowing = true;
             // ToDo: actually throw chips accumulated over the course of the game
+            for (int i = 0; i < totalAddedChips; i++)
+            {
+                GameObject chip = Instantiate(chipPrefab, chipThrowPos.position, Maf.GetRandomRotation());
+                Vector3 randomForce = new Vector3(Random.Range(-1f, 1f), Random.Range(0.2f, 0.6f), Random.Range(-1f, 1f)) * throwForce;
+                chip.GetComponent<Rigidbody>().AddForce(randomForce, ForceMode.Impulse);
+                Destroy(chip, 10f);
+                QOL.GetWaitForSeconds(0.1f);
+            }
             yield return null;
             IsThrowing = false;
         }
